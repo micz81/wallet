176382ffeea852ea7ef1e9cdab0aa11ad6e5043e
micz81 <mzaborski81@gmail.com>
2023-07-17 16:36:36 +0200

    edit button in progress

--- src/main/java/com/wallet/service/StockPricingService.java
+++ src/main/java/com/wallet/service/StockPricingService.java
@@ -1,5 +1,48 @@
 package com.wallet.service;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
 public class StockPricingService {
     //seeking alpha from rapidapi
+
+
+    public int getId(String ticker) throws IOException {
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://seeking-alpha.p.rapidapi.com/v2/auto-complete?query=apple&type=people%2Csymbols%2Cpages&size=5")
+                .get()
+                .addHeader("X-RapidAPI-Key", "7b4458f0c9mshd301be4ebadf3f2p11c14djsn714398223539")
+                .addHeader("X-RapidAPI-Host", "seeking-alpha.p.rapidapi.com")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        return 146;
+    }
+    public void getPrice() throws IOException {
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://seeking-alpha.p.rapidapi.com/market/get-realtime-quotes?sa_ids=146")
+                .get()
+                .addHeader("X-RapidAPI-Key", "7b4458f0c9mshd301be4ebadf3f2p11c14djsn714398223539")
+                .addHeader("X-RapidAPI-Host", "seeking-alpha.p.rapidapi.com")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        System.out.println(response);
+
+    }
+
+
+    public StockPricingService() throws IOException {
+    }
 }
--- src/test/java/com/wallet/ApplicationTests.java
+++ src/test/java/com/wallet/ApplicationTests.java
@@ -1,13 +1,25 @@
 package com.wallet;
 
+import com.wallet.service.StockPricingService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
 
 @SpringBootTest
 class ApplicationTests {
 
+	@Autowired
+	StockPricingService stockPricingService;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void getPrice() throws IOException {
+		stockPricingService.getPrice();
 	}
 
 }
